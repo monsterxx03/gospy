db7575d34293547661c355bba3d8e832010d284f
monsterxx03 <xyj.asmy@gmail.com>
2025-04-12 15:20:42 +0800

    refactor: add comment for Goroutines func behavior

--- pkg/proc/mem_reader.go
+++ pkg/proc/mem_reader.go
@@ -8,6 +8,7 @@ type ProcessMemReader interface {
 	io.ReaderAt
 	Close() error
 	RuntimeInfo() (*Runtime, error)
+	// ai! add showDead option to Goroutines func, by default, this func will filter goroutines in dead status
 	Goroutines() ([]G, error)
 	GetGoroutineStackTraceByGoID(goid int64) ([]StackFrame, error)
 	Ps() ([]P, error)
